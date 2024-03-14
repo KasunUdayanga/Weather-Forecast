876febf71d2a485a7efe54e11e974ce43bcfef10
Kasun_Udayanga <udayangakasun696@gmail.com>
2024-03-14 23:01:29 +0530

    small apikey}&city=${city}apikey}&city=${city}

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function getWeather(){
-    const apikey = '47c6a200841b1d4a6720b31e29dff310';
+    const apikey = '4ab8621582c2c478d5b662b653ef717e';
     const city = document.getElementById('city').value;
     if (!city) {
         alert('Please enter city name');
         return;
     }
     const currenturl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-    const forecasturl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`;
+    const forecasturl = `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${apikey}`;
     fetch(currenturl)
-   .then(response => response.json())
-   .then(data=>{
-    displayWeather(data);
+            .then(response => response.json())
+            .then(data =>{
+                    displayWeather(data);
    })
-   .catch(err =>{
-    console.log(err);
-    alert("Error fetching weather");
+            .catch(err =>{
+                console.log(err);
+                alert("Error fetching weather");
    });
 
    fetch(forecasturl)
-   .then(response => response.json())
-   .then(data=>{
-    displayHourlyForecast(data.list);
+            .then(response => response.json())
+            .then(data=>{
+                    displayHourlyForecast(data.list);
    })
-   .catch(err =>{
-    console.log(err);
-    alert("Error fetching Hourly Forecast weather");
+            .catch(err =>{
+                 console.log(err);
+                 alert("Error fetching Hourly Forecast weather");
    });
 
 }
@@ -46,10 +46,10 @@ function displayWeather(data){
         const temp=Math.round(data.main.temp - 273.15);
         const description=data.weather[0].description;
         const icon=data.weather[0].icon;
-        const icon_url=`http://openweathermap.org/img/wn/${icon}@2x.png`;
+        const icon_url=`https://openweathermap.org/img/wn/${icon}@2x.png`;
         const tempHTML=`<p>${temp}°C</p>`;
         const weatherHtml=`<p>${cityname}</p>
-        <p>${description}</p>`;
+                            <p>${description}</p>`;
         tempDivInfo.innerHTML=tempHTML;
         weatherInfoDiv.innerHTML=weatherHtml;
         weatherIcon.src=icon_url;
@@ -60,13 +60,13 @@ function displayWeather(data){
 
 function displayHourlyForecast(hourlyData) {
     const hourlyForecastDiv=document.getElementById('hourlyForecast');
-    const next24Hours=hourlyData.slice(0.8);
-    next24Hours.forEach(item=>{
+    const next24Hours=hourlyData.slice(0,8);
+    next24Hours.forEach(item =>{
         const dateTime=new Date(item.dt*1000);
         const hour=dateTime.getHours();
         const temperature=Math.round(item.main.temp - 273.15);
         const iconcode=item.weather[0].icon;
-        const icon_url=`http://openweathermap.org/img/wn/${iconcode}@2x.png`;
+        const icon_url=`https://openweathermap.org/img/wn/${iconcode}@2x.png`;
         const hourlyItemHtml=`<div class="hourly-item">
         <span>${hour}:00</span>
         <img src="${icon_url}" alt="Hourly Weather Icon">
